e59783fba35abe888f90c32413bc5174df5ea357
Joshua L Rasmussen <josh.rasmussen@discorp.com>
2016-11-09 11:36:50 -0800

    Initial Commit

---
+++ JRAuditableEntity/JRAuditableEntity.h
@@ -0,0 +1,19 @@
+//
+//  JRAuditableEntity.h
+//  JRAuditableEntity
+//
+//  Created by Joshua L Rasmussen on 11/9/16.
+//  Copyright © 2016 Dealer Information Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JRAuditableEntity.
+FOUNDATION_EXPORT double JRAuditableEntityVersionNumber;
+
+//! Project version string for JRAuditableEntity.
+FOUNDATION_EXPORT const unsigned char JRAuditableEntityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JRAuditableEntity/PublicHeader.h>
+
+
